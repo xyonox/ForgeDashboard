d5f8d743969dbeeee6d69de47457b462b29c498c
Carl <devxyonox@gmail.com>
2026-06-28 17:40:40 +0200

    check if user is logged in

--- pages/main_page.py
+++ pages/main_page.py
@@ -4,7 +4,8 @@ import services.user_service as us
 
 @ui.page("/")
 def mainpage():
-    us.is_logged_in()
+    if not us.is_logged_in():
+        return
 
     eui.header()
     ui.label("Hello " + app.storage.user["username"])
