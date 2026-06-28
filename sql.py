18e5974f748074fb621d156b0ff541a1b16beb1a
Carl <devxyonox@gmail.com>
2026-06-28 17:41:02 +0200

    create new models

--- sql.py
+++ sql.py
@@ -4,14 +4,6 @@ from peewee import *
 def get_db():
     return SqliteDatabase('forge.db')
 
-class CodingSession(Model):
-
-    daytime = DateField(unique=True)
-    minutes = IntegerField()
-
-    class Meta:
-        database = get_db()
-
 class TestUser(Model):
     username = CharField(unique=True)
     password = CharField()
@@ -27,6 +19,7 @@ class User(Model):
         database = get_db()
 
 class Exercise(Model):
+    user = ForeignKeyField(User, backref='exercises')
     name = CharField()
     description = TextField()
     class Meta: database = get_db()
@@ -38,6 +31,7 @@ class Weekday(Model):
 
 # DIE ZWISCHENTABELLE
 class ExerciseWeekday(Model):
+    user = ForeignKeyField(User, backref='workout_plans')
     exercise = ForeignKeyField(Exercise, backref='days')
     weekday = ForeignKeyField(Weekday, backref='exercises')
 
@@ -49,8 +43,10 @@ class ExerciseWeekday(Model):
         database = get_db()
         # verhindert, dass gleiche workout_position gleichzeitig existieren
         indexes = (
-            (('weekday', 'workout_position'), True),
+            (('user' ,'weekday', 'workout_position'), True),
         )
+
+
 
 def init_db():
     print("Initializing database")
@@ -59,10 +55,22 @@ def init_db():
     print("Creating tables")
 
     try:
-        db.create_tables([CodingSession, TestUser, User])
+        tables_to_create = [
+            TestUser,
+            User,
+            Exercise,
+            Weekday,
+            ExerciseWeekday
+        ]
+        db.create_tables(tables_to_create, safe=True)
         print("Tables created")
+
+        for i in range(7):
+            Weekday.get_or_create(day_index=i)
+        print("Weekdays initialized (0-6)")
     except:
-        print("Tables already exist")
+        print("something went wrong:")
+        print(db.get_tables())
     print("Creating test variables")
     # Bei create wird es versucht immer wieder neu zu erstellen, bei get_or_create nur wenn es nicht existiert
     TestUser.get_or_create(username="test", password="Sollte zensiert werden")
